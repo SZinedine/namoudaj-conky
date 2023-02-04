656d76c7cbd74349ba75b055bf5f5c2cfc1e0fce
SZinedine <saibi.zineddine@yahoo.com>
2023-02-04 20:46:34 +0100

    extracted the colors into its own file and changed the system of color choosing.

--- start.lua
+++ start.lua
@@ -7,6 +7,8 @@
 -- Repository:  https://www.github.com/SZinedine/namoudaj-conky
 ----------------------------------
 
+require("abstract")
+
 -- This is a Demo
 -- replace the content of the following function to create your own conky theme
 function start()
@@ -14,27 +16,27 @@ function start()
     local thickness = 3
 
     ring_anticlockwise(277, 200, rings_radius, thickness, 400, 140, cpu_percent(), 100, color_frompercent(tonumber(cpu_percent())))   -- cpu ring
-    ring_clockwise(555, 200, rings_radius, thickness, 0, 360, cpu_temperature(), 100, main_fg)     -- temperature ring
-    ring_clockwise(833, 200, rings_radius, thickness, -40, 220, memory_percent(), 100, main_fg)    -- ram ring
+    ring_clockwise(555, 200, rings_radius, thickness, 0, 360, cpu_temperature(), 100, colors.fg)     -- temperature ring
+    ring_clockwise(833, 200, rings_radius, thickness, -40, 220, memory_percent(), 100, colors.fg)    -- ram ring
 
     if has_battery then
         rectangle_leftright(160, 400, 800, thickness, battery_percent(), 100, color_frompercent_reverse(tonumber(battery_percent())))   -- battery rectangle / line
-        write(535, 420, "Battery", 14, main_text_color)
-        write(125, 407, battery_percent() .. "%", 14, main_text_color)
+        write(535, 420, "Battery", 14, colors.text)
+        write(125, 407, battery_percent() .. "%", 14, colors.text)
     end
 
     -- titles
-    write(267, 370, "CPU", 14, main_text_color)
-    write(515, 370, "Temperature", 14, main_text_color)
-    write(813, 370, "Memory", 14, main_text_color)
+    write(267, 370, "CPU", 14, colors.text)
+    write(515, 370, "Temperature", 14, colors.text)
+    write(813, 370, "Memory", 14, colors.text)
 
     -- text values
-    write(267, 210, cpu_percent() .. "%", 14, main_text_color)
-    write(545, 210, cpu_temperature() .. "°C", 14, main_text_color)
-    write(823, 210, memory_percent() .. "%", 14, main_text_color)
+    write(267, 210, cpu_percent() .. "%", 14, colors.text)
+    write(545, 210, cpu_temperature() .. "°C", 14, colors.text)
+    write(823, 210, memory_percent() .. "%", 14, colors.text)
 
     -- clock
-    write(430, 530, time_hrmin(), 80, main_text_color)
+    write(430, 530, time_hrmin(), 80, colors.text)
 end
 
 
@@ -42,8 +44,6 @@ end
 function conky_main()
     if conky_window == nil then
         return
-    elseif colors_defined == false then
-        io.stderr:write("Fatal Error. Please define a theme")
     end
     local cs = cairo_xlib_surface_create(conky_window.display, conky_window.drawable,
                                          conky_window.visual, conky_window.width,
